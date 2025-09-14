c5fd42f8b0086d86c7885716bba00f62fb091bf8
Maaz-Bin-Haider <maazbinhaider2024@gmail.com>
2025-09-14 07:44:58 +0500

    added base.html file

--- parties/views.py
+++ parties/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def create_new_party(request):
+    if request.method == 'POST':
+        party_name = request.POST.get('party_name')
+        party_type = request.POST.get('party_type')
+        contact_info = request.POST.get('contact_info')
+        address = request.POST.get('address')
+        opening_balance = request.POST.get('opening_balance')
+        balance_type = request.POST.get('balance_type')
+
+        print(party_name,party_type,contact_info,address,opening_balance,balance_type)
+
     return render(request,"parties_templates/add_new_party.html")
