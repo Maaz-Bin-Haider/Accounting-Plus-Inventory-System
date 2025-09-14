ccb72859a7191e368fb78c8af650a95f38ccdbc0
Maaz-Bin-Haider <maazbinhaider2024@gmail.com>
2025-09-14 07:23:31 +0500

    Added template, view and url for adding parties

--- parties/urls.py
+++ parties/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from .views import create_new_party
+
 urlpatterns = [
-    
+    path('add-new-party/',create_new_party,name='add_new_party'),
 ]
--- parties/views.py
+++ parties/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def create_new_party(request):
+    return render(request,"parties_templates/add_new_party.html")
