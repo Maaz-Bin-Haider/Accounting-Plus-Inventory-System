e6c7d4f1ef898f2e0c012520ba3e9c139b8acc12
Apple <apple@Apples-MacBook-Air.local>
2025-10-08 18:21:37 +0500

    logic for validating purchase data before excuting DB functions

--- purchase/views.py
+++ purchase/views.py
@@ -11,7 +11,7 @@ def purchasing(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            print(data)
+            # print(data)
             # validation example
             if not data.get("party_name"):
                 return JsonResponse({"success": False, "message": "Party name is required."})
@@ -46,12 +46,17 @@ def purchasing(request):
             except (ValueError, TypeError):
                 return JsonResponse({"success": False, "message": "Invalid date. Please enter a valid date in YYYY-MM-DD format."})
             
+            # validate Items
             try: 
                 for item in data.get("items"):
                     item_name = item["item_name"]
                     qty = item["qty"]
                     unit_price = item["unit_price"]
                     serials = item["serials"]
+
+                    print(qty,type(qty))
+                    print(unit_price,type(unit_price))
+                    print(serials,type(serials))
 
                     # validating item name
                     with connection.cursor() as cursor:
@@ -61,7 +66,73 @@ def purchasing(request):
                         if not exists:
                             return JsonResponse({"success": False, "message": f"Item with '{item_name.upper()}' Not exists!"})
                         
+                    # validating quantity 
+                    try:
+                        qty = int(qty)
+                        if qty <= 0:
+                            return JsonResponse({"success": False, "message": "Invalid Quantity"})
+                    except:
+                        return JsonResponse({"success": False, "message": "Invalid Quantity"})
 
+                    # validating unit price
+                    try:
+                        unit_price = float(unit_price)
+                        if unit_price <= 0:
+                            return JsonResponse({"success": False, "message": "Invalid Price"})
+                    except:
+                        return JsonResponse({"success": False, "message": "Invalid Price"})
+                    
+
+                    # Validating Serials
+                    try:
+                        for serial in serials:
+
+                            with connection.cursor() as cursor:
+                                cursor.execute("SELECT in_stock FROM get_serial_number_details(%s)",[serial])
+
+                                exists = cursor.fetchone()
+
+                                if exists:
+                                    return JsonResponse({"success": False, "message": f"The Serial '{serial}' already exists in Stock!"})
+                    except:
+                        return JsonResponse({"success": False, "message": "Invalid Serial Number!"})
+                    
+            except:
+                pass
+            
+            # Execute DB function
+            try:
+                print('Enter Execution Block------')
+                # 1️⃣ Find the vendor ID
+                with connection.cursor() as cursor:
+                    cursor.execute("""
+                        SELECT party_id 
+                        FROM Parties 
+                        WHERE party_name = %s
+                    """, [data.get("party_name")])
+                    result = cursor.fetchone()
+                    if not result:
+                        return JsonResponse({"success": False, "message": f"Party '{data.get("party_name")}' not found in Parties."})
+                    vendor_id = result[0]
+                    
+                    
+                    # Prepare your purchase items data
+                    items_data = []
+                    for item in data.get("items"):
+                        items_data.append(item)
+                    print(items_data,'----ooe-----',vendor_id)
+
+            
+                # Convert Python list → JSON string
+                items_json = json.dumps(items_data)
+
+                # # 3️⃣ Call the Postgres function `create_purchase`
+                # cursor.execute("""
+                #     SELECT create_purchase(%s, %s, %s::jsonb);
+                # """, [vendor_id, date.today(), items_json])
+
+                # # 4️⃣ Fetch the returned invoice ID
+                # invoice_id = cursor.fetchone()[0]
             except:
                 pass
             
