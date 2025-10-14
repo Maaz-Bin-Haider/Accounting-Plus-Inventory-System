39960401d53b8ec324128308e0746591fa94e5a8
Maaz-Bin-Haider <maaz.rehan2020@gmail.com>
2025-10-14 13:26:28 +0500

    item name detection from serial number and exception handling when serial doesn't exists in stock while making purchase return is added

--- purchaseReturn/urls.py
+++ purchaseReturn/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import createPurchaseReturn
+from .views import createPurchaseReturn,purchase_return_lookup
 
 app_name = "purchaseReturn"
 
 urlpatterns = [
     path('create-purchase-return/',createPurchaseReturn, name="create_purchase_return"),
+    path('lookup/<str:serial>/',purchase_return_lookup,name="purchase_return_lookup"),
 ]
--- purchaseReturn/views.py
+++ purchaseReturn/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+from django.db import connection
+from django.http import JsonResponse
 
 # Create your views here.
 
 def createPurchaseReturn(request):
     return render(request,'purchase_return_templates/purchase_return_template.html')
+
+
+def purchase_return_lookup(request,serial:str):
+
+    #validating Serial Number
+    try:
+        serial = str(serial)
+    except:
+        return JsonResponse({ "success": False, "message":"Invalid Serial Number" })
+    
+    # checking in Current Stock
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT in_stock FROM get_serial_number_details(%s)",[serial])
+            exists = cursor.fetchone()
+            if not exists[0]:
+                return JsonResponse({ "success": False, "message":f"The Serial '{serial}' does not exists in stock!" })
+            
+            cursor.execute("SELECT item_name FROM get_serial_number_details(%s)",[serial])
+            item_name = cursor.fetchone()
+    except Exception as e:
+        return JsonResponse({ "success": False, "message":f"The Serial '{serial}' does not exists in stock!" })
+    
+    return JsonResponse({ "success": True, "item_name": item_name[0] })
+    
