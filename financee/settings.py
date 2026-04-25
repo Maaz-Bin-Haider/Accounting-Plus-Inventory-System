4d0c60060567f72a2e6bb36bd572c70772b61c63
Maaz-Bin-Haider <maazbinhaider2024@gmail.com>
2026-04-25 21:21:46 +0500

    edit settings for dynamic cache clear

--- financee/settings.py
+++ financee/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
